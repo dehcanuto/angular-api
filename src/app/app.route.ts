1f95692353b48350e224eaf0f64d61e76d319423
dehcanuto <andrecanutoc@gmail.com>
2021-12-03 17:56:11 -0300

    2 files modified

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { HomeModule } from './home/home.module';
 import { AppComponent } from './app.component';
 import { SingleComponent } from './home/single/single.component';
 
